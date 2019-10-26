7a1f4272528f61bb922fe07f0bc7693c27b79227
Colin Zhou <colinzhou@berkeley.edu>
2019-10-26 01:20:30 -0700

    First commit of adding frontend

---
+++ webapp/src/loginscreen/LoginScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+
+export default class PlanScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+
+    )
+  }
+
+}
