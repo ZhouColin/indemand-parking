957a58fab017cd0d662a3f2268680874d0fee693
David Bian <dbian@berkeley.edu>
2019-10-26 16:17:56 -0700

    DBSCAN revised

--- mlService/MLIntelligence.py
+++ mlService/MLIntelligence.py
@@ -19,40 +19,45 @@ def getData():
 # No need to store return values as a json file, this will be done by caller
 def clustering(data):
 
-	database = pd.DataFrame(list(JSON.parse(data).items))
+	database = pd.DataFrame(list(JSON.parse(data).items()))
+	'''
+		Contents of the database are ChurnRecord Class
+		ChurnRecord Class:
+			double lon;
+	    	double lat;
+	    	long time;
+	'''
 
 	supply = database[:0]
+	demand = database[:1]
 
-	demandCoords = zip([c.lat for c in supply], [c.lon for c in supply])
-	demandCoords = pd.as_matrix(demandCoords)
-	
-	'''
-	Contents of the database are ChurnRecord Class
-	ChurnRecord Class:
-		double lon;
-    	double lat;
-    	long time;
-    '''
-	#miles per radian
-	mpr = 3963.20488348
-	epsilon = 1.5 / mpr
-	dbObj = DBSCAN(eps=epsilon, min_samples=1, algorithm='ball_tree', metric='haversine')
-	db = dbObj.fit(np.radians(demandCoords))
-	cluster_labels = db.labels_
-	num_clusters = len(set(cluster_labels))
-	clusters = pd.Series([demandCoords[cluster_labels == n] for n in range(num_clusters)])  #unsure about this line...seems like youre just taking the first n entries in demandCoords?
-	#print('Number of clusters: {}'.format(num_clusters))
+	def findCluster(coordinateList):
+
+		coords = zip([c.lat for c in coordinateList], [c.lon for c in coordinateList])
+		coords = pd.as_matrix(coords)
+		
+		
+		#miles per radian
+		mpr = 3963.20488348
+		epsilon = 1.5 / mpr
+		dbObj = DBSCAN(eps=epsilon, min_samples=1, algorithm='ball_tree', metric='haversine')
+		db = dbObj.fit(np.radians(coords))
+		cluster_labels = db.labels_
+		num_clusters = len(set(cluster_labels))
+		clusters = pd.Series([coords[cluster_labels == n] for n in range(num_clusters)])  #unsure about this line...seems like youre just taking the first n entries in coords?
+		#print('Number of clusters: {}'.format(num_clusters))
 
 
-	def get_centermost_point(cluster):
-    	centroid = (MultiPoint(cluster).centroid.x, MultiPoint(cluster).centroid.y)
-    	centermost_point = min(cluster, key=lambda point: great_circle(point, centroid).m)
-    	return tuple(centermost_point)
+		def getCenter(cluster):
+	    	centroid = (MultiPoint(cluster).centroid.x, MultiPoint(cluster).centroid.y)
+	    	centermost_point = min(cluster, key=lambda point: great_circle(point, centroid).m)
+	    	return tuple(centermost_point)
 
-	centermost_points = clusters.map(get_centermost_point)
+		centermost_points = clusters.map(getCenter)
 
+		return list(centermost_points)
 
-	return centermost_points
+	return [findCluster(supply), findCluster(demand)]
 
 	'''
 	lats, lons = zip(*centermost_points)
