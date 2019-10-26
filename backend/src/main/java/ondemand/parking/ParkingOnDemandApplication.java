a092081f7de2bfbe131822d131443e01272bf801
Justin Wei <justinwei2@gmail.com>
2019-10-25 20:16:58 -0700

    project start

---
+++ backend/src/main/java/ondemand/parking/ParkingOnDemandApplication.java
@@ -0,0 +1,13 @@
+package ondemand.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingOnDemandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingOnDemandApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ondemand/parking/ParkingOnDemandApplicationTests.java
@@ -0,0 +1,13 @@
+package ondemand.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingOnDemandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
