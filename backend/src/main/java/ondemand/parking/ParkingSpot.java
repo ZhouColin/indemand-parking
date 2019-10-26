db6e4bf1e2c8c3d47600562c8690f5ad38961663
David Bian <dbian@berkeley.edu>
2019-10-26 00:31:08 -0700

    reserve method

--- backend/src/main/java/ondemand/parking/ParkingSpot.java
+++ backend/src/main/java/ondemand/parking/ParkingSpot.java
@@ -2,15 +2,19 @@ package ondemand.parking;
 
 public class ParkingSpot {
     String psID;
+    String ownerId;
     double[] location;
+    double meterRate;
     long time;
     long duration;
     boolean taken;
 
-    ParkingSpot(double[] location, long time, long duration) {
+
+    ParkingSpot(double[] location, long time, long duration, double m) {
         this.location = location;
         this.time = time;
         this.duration = duration;
+        this.meterRate = m;
 
         ParkingOnDemandApplication.db.addParkingSpot(this);
     }
--- backend/src/main/java/ondemand/parking/User.java
+++ backend/src/main/java/ondemand/parking/User.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.UUID;
 
 @RestController
@@ -14,6 +16,8 @@ public class User {
     String psID;
     double rating;
     double raters;
+    ArrayList<String> psToRate;
+
 
     User() {}
 
@@ -23,20 +27,37 @@ public class User {
         raters = 0;
 
         ParkingOnDemandApplication.db.addUser(this);
+
+        psToRate = new ArrayList<String>();
     }
 
+    //reserve a parking spot
+    static ResponseEntity<String> reserve(@RequestParam String userName, @RequestParam String psID) {
+        ParkingSpot spot = db.getParkingSpot(psID);
+        User user = db.getUser(userName);
+        spot.taken = true;
+        user.psToRate.add(psID);
 
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
     //rate function, used for someone that wants to rate the seller
     @GetMapping("/rate")
-    static ResponseEntity<String> rate(@RequestParam String uid, @RequestParam double rating) {
+    static ResponseEntity<String> rate(@RequestParam String uID, @RequestParam String psID, @RequestParam double rating) {
+
         if(rating < 0 || rating > 5) {
             return new ResponseEntity<>("Rating must be between 0 to 5", HttpStatus.CONFLICT);
         }
 
-        User target = db.getUser(uid);
+        User user = db.getUser(uID);
+        ParkingSpot spot = db.getParkingSpot(psID);
+        String targetID = spot.ownerId;
+        User target = db.getUser(targetID);
+
         double newRating = (target.rating * target.raters + rating)/(target.raters + 1);
         target.rating = newRating;
         target.raters++;
+
+        user.psToRate.remove(psID);
 
         return new ResponseEntity<String>(Double.toString(target.rating), HttpStatus.OK);
         //after reservation
