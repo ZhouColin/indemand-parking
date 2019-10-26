06265d1b9d05524ba12220337b20185d64500bef
saurav_c <sauravc@berkeley.edu>
2019-10-25 22:10:43 -0700

    Basic Login

--- backend/src/main/java/ondemand/parking/User.java
+++ backend/src/main/java/ondemand/parking/User.java
@@ -1,13 +1,21 @@
 package ondemand.parking;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
+@RestController
 public class User {
     static Database db = ParkingOnDemandApplication.db;
     String uID;
     String psID;
     double rating;
     double raters;
+
+    User() {}
 
     User(String username, String password) {
         uID = UUID.nameUUIDFromBytes((username + password).getBytes()).toString();
@@ -18,12 +26,14 @@ public class User {
     }
 
     // Login function, creates a new user if necessary
-    static String getUserID(String username, String password) {
+    @GetMapping("/login")
+    static ResponseEntity<String> login(@RequestParam String username, @RequestParam String password) {
         String uID = UUID.nameUUIDFromBytes((username + password).getBytes()).toString();
 
         if (!db.userExists(uID)) {
             new User(username, password);
+            return new ResponseEntity<>(uID, HttpStatus.OK);
         }
-        return uID;
+        return new ResponseEntity<>("Username In Use", HttpStatus.BAD_REQUEST);
     }
 }
