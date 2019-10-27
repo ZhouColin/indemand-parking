e1fd804d46c79a4b858edf2fb7c1a768fdd4010d
Colin Zhou <colinzhou@berkeley.edu>
2019-10-26 22:24:24 -0700

    update for react routing

---
+++ sdl-parking/sdl-parking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sdl-parking
+//
+//  Created by Colin Zhou on 2019-10-26.
+//  Copyright © 2019 Colin Zhou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import Sidebar from "react-sidebar";
 
 const mapStyles = {
   width: '100%',
@@ -16,8 +17,14 @@ export class MapContainer extends Component {
               {latitude: 47.2052192687988, longitude: -121.988426208496},
               {latitude: 47.6307081, longitude: -122.1434325},
               {latitude: 47.3084488, longitude: -122.2140121},
-              {latitude: 47.5524695, longitude: -122.0425407}]
+              {latitude: 47.5524695, longitude: -122.0425407}],
+      sidebarOpen: false
     }
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
   }
 
   displayMarkers = () => {
@@ -26,7 +33,7 @@ export class MapContainer extends Component {
        lat: store.latitude,
        lng: store.longitude
      }}
-     onClick={() => console.log("You clicked me!")} />
+     <onClick={() => this.onSetSidebarOpen(true)} />
     })
   }
 
@@ -39,7 +46,18 @@ export class MapContainer extends Component {
           initialCenter={{ lat: 47.444, lng: -122.176}}
         >
           {this.displayMarkers()}
+          <Sidebar
+            sidebar={<b>Sidebar content</b>}
+            open={this.state.sidebarOpen}
+            onSetOpen={this.onSetSidebarOpen}
+            styles={{ sidebar: { background: "white" } }}
+          >
+            <button onClick={() => this.onSetSidebarOpen(true)}>
+              Open sidebar
+            </button>
+          </Sidebar>
         </Map>
+
     );
   }
 }
